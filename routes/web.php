f507faf3c176627df2daf655e4da89afc9e3ed61
WojciechBiegun <wojciechbiegun7@gmail.com>
2021-11-28 22:33:15 +0100

    dodanie funkcjonalności konsultanta do aplikacji

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,12 @@ use App\Http\Controllers\MainController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-<<<<<<< HEAD
 })
 ->middleware(['auth'])
 ->name('dashboard');
 Route::get('/dashboard-konsultant',function (){
   return view ('/dashboard-konsultant');
 })->middleware(['auth:konsultant','auth'])->name('dashboard-konsultant');
-=======
-})->middleware(['auth'])->name('dashboard');
-
-
-//Route to user array 
-Route::get('/',[MainController::class, 'index']);
-
-
-
-
-Route::get('/login',[MainController::class, 'login'])->name('login');
->>>>>>> b00bf56201a3130fe1e7a35521644f8b71afb253
 require __DIR__.'/auth.php';
+//Route to user
+Route::get('/',[MainController::class,'index']);
